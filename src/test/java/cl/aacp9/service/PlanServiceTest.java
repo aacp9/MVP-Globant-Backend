0e1c06eb8bd9e1f9b6f3dd628469336c0bf97e07
aacp9 <aacp9439@gmail.com>
2025-11-25 18:35:05 -0300

    subiendo mas test mockito

--- src/test/java/cl/aacp9/service/PlanServiceTest.java
+++ src/test/java/cl/aacp9/service/PlanServiceTest.java
@@ -13,43 +13,39 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import cl.aacp9.repository.IClienteRepository;
+import cl.aacp9.repository.IPlanRepository;
 
 @ExtendWith(MockitoExtension.class)
-class ClienteServiceTest {
-
-	//interface que se le hará el test
-	@InjectMocks 
-	private IClienteService clienteServiceTest = new ClienteServiceImpl();
+public class PlanServiceTest {
 	
-	//simula objeto 
-	@Mock 
-	private IClienteRepository clienteRepositoryTest;
+	@InjectMocks
+	private IPlanService planServiceTest = new PlanServiceImpl();
+	
+	@Mock
+	private IPlanRepository planRepositoryTest;
 	
 	//happyPathTest
 	@Test
 	void ShouldReturnTrue() {
-		//
-		when (clienteRepositoryTest.existsById(anyInt())).thenReturn(true);
-		assertTrue(clienteServiceTest.existeCliente(anyInt()));
-
+		when (planRepositoryTest.existsById(anyInt())).thenReturn(true);
+		assertTrue(planServiceTest.existePlan(anyInt()));
+		
 		//se verifica la ejecucion del metodo del repositorio(mock)
-		verify(clienteRepositoryTest, times(1)).existsById(anyInt());
+		verify(planRepositoryTest, times(1)).existsById(anyInt());
 	}
 
 	//FalsePathTest
 	@Test
-	void shouldReturnFalse() {
-		when (clienteRepositoryTest.existsById(anyInt())).thenReturn(false);
-		assertFalse(clienteServiceTest.existeCliente(anyInt()));
+	void ShouldReturnFalse() {
+		when (planRepositoryTest.existsById(anyInt())).thenReturn(false);
+		assertFalse(planServiceTest.existePlan(anyInt()));
 		
 		//se verifica la ejecucion del metodo del repositorio(mock)
 		//el verify se utiliza cuando el método no retorna nada, o sea no tenemos como comparar un resultado
 		//para comprobar si ejecuto correctamente, en tal caso usamo verify para al menos comprobar que el metodo 
 		//es realizado sin errores
-		verify(clienteRepositoryTest, times(1)).existsById(anyInt());
+		verify(planRepositoryTest, times(1)).existsById(anyInt());
 	}
-	
-	
+
 
 }
